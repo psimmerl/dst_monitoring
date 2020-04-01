77c1eeb7f3fdebe2710720f561caa06fa86d2a4a
Andrey <kenjo@jlab.org>
2020-03-31 22:14:44 -0400

    ecounts update

--- clas12/mon/electron/ECounts.groovy
+++ clas12/mon/electron/ECounts.groovy
@@ -1,87 +1,76 @@
-#!/home/kenjo/.groovy/coatjava/bin/run-groovy
-import org.jlab.io.hipo.HipoDataSource
-import org.jlab.detector.base.DetectorType
-import org.jlab.clas.physics.Vector3
+package clas12.mon.electron
+
+import org.jlab.clas.physics.LorentzVector
 import org.jlab.groot.data.H1F
 import org.jlab.groot.data.H2F
-import org.jlab.groot.data.TDirectory
-import groovyx.gpars.GParsPool
 import java.util.concurrent.ConcurrentHashMap
-import java.util.concurrent.Executors
-import java.util.concurrent.ScheduledExecutorService
-import java.util.concurrent.TimeUnit
-import java.util.concurrent.atomic.AtomicInteger
 
-MyMods.enable()
-/////////////////////////////////////////////////////////////////////
 
-def outname = args[0].split('/')[-1]
+class ECounts {
+  def hists = new ConcurrentHashMap()
+  def fcentry = new ConcurrentHashMap()
+  def elentry = new ConcurrentHashMap()
 
-def timeline = new ConcurrentHashMap()
+  def processEvent(event) {
+    if(event.hasBank("RUN::config")) {
+      def cnfb = event.getBank("RUN::config")
+      def ts = cnfb.getLong("timestamp", 0)
 
-GParsPool.withPool 12, {
-  args.eachParallel{fname->
-    def reader = new HipoDataSource()
-    reader.open(fname)
+      if(event.hasBank("RUN::scaler")) {
+        def scaler = event.getBank("RUN::scaler")
+        def fcg = scaler.getFloat('fcupgated',0)
+        if(fcentry.contains(ts)) println([fcentry[ts], fcg])
+        fcentry[ts] = fcg
+      }
 
-    //while(reader.hasEvent() && evcount.get()<5000000) {
-    while(reader.hasEvent()) {
-      def event = reader.getNextEvent()
-      if(event.hasBank("RUN::config")) {
-        def cnfb = event.getBank("RUN::config")
-        def ts = cnfb.getLong("timestamp", 0)
+     if(event.hasBank("REC::Particle")) {
+        def partb = event.getBank("REC::Particle")
+        def iele = (0..<partb.rows()).find{partb.getInt('pid',it)==11 && partb.getShort("status",it)<0}
+        if(elentry.contains(ts)) println(cnfb)
+        if(iele!=null) elentry[ts] = 0
+      }
+    }
+  }
 
-        if(event.hasBank("REC::Particle")) {
-          def partb = event.getBank("REC::Particle")
-          def iele = (0..<partb.rows()).find{partb.getInt('pid',it)==11 && partb.getShort("status",it)<0}
-          if(iele!=null) timeline[ts] = [1,1]
-        }
 
-        if(event.hasBank("RUN::scaler")) {
-          def scaler = event.getBank("RUN::scaler")
-          timeline[ts] = [scaler.getFloat('fcup',0), scaler.getFloat('fcupgated',0)]
-        }
-      }    
+  def finish() {
+/*
+    def fc0 = null, fc1 = null, ts0 = null
+
+    def timeline = fcentry.sort().collect{ts,fcs->[ts,*fcs]}
+
+    def data = (0..timeline.size-2).collect{i->
+      def (dt,dfc0,dfc1) = [0,1,2].collect{timeline[i+1][it]-timeline[i][it]}
+      dt *= 4*1e-9
+      return [dt, dfc0, dfc1, dfc0/dt, dfc1/dt]
     }
 
-    reader.close()
+    def h0fcdt = new H1F("hdt_0", "time between FC readings;time [sec]", 200,0,data.max{it[0]}[0])
+    def h0fcdq0 = new H1F("hugtdQ_0", "ungated charge between FC readings:charge [nC]", 300,0,data.max{it[1]}[1])
+    def h0fcdq1 = new H1F("hgtdQ_0", "gated charge between FC readings:charge [nC]", 300,0,data.max{it[2]}[2])
+    def h0curr0 = new H1F("hugtdI_0", "ungated current;current [nA]", 200,0,data.max{it[3]}[3])
+    def h0curr1 = new H1F("hgtdI_0", "gated current;current [nA]", 200,0,data.max{it[4]}[4])
+    def h1fcdt = new H1F("hdt_1", "time between FC readings;time [sec]", 200,0,0.1)
+    def h1fcdq0 = new H1F("hugtdQ_1", "charge between FC readings:charge [nC]", 300,0,3)
+    def h1fcdq1 = new H1F("hgtdQ_1", "charge between FC readings:charge [nC]", 300,0,3)
+    def h1curr0 = new H1F("hugtdI_1", "current;current [nA]", 300,0,60)
+    def h1curr1 = new H1F("hgtdI_1", "current;current [nA]", 300,0,60)
+
+    data.each{dt,dfc0,dfc1,curr0,curr1->
+      h0fcdt.fill(dt)
+      h0fcdq0.fill(dfc0)
+      h0fcdq1.fill(dfc1)
+      h0curr0.fill(curr0)
+      h0curr1.fill(curr1)
+
+      h1fcdt.fill(dt)
+      h1fcdq0.fill(dfc0)
+      h1fcdq1.fill(dfc1)
+      h1curr0.fill(curr0)
+      h1curr1.fill(curr1)
+    }
+
+    [h0fcdt,h0fcdq0,h0fcdq1,h0curr0,h0curr1,h1fcdt,h1fcdq0,h1fcdq1,h1curr0,h1curr1].each{hists[it.getName()] = it}
+*/
   }
 }
-
-
-def fc0 = null, fc1 = null, ts0 = null
-def nelemap = [:].withDefault{0}
-
-timeline.sort().each{ts,fcs->
-  if(fcs[0]==1 && fc0!=null) {
-    nelemap[[ts0,fc0,fc1]]++
-  } else if(fcs[0]!=1) {
-    ts0 = ts
-    fc0 = fcs[0]
-    fc1 = fcs[1]
-  }
-}
-
-def heyield = new H1F("heyield", "ele yield", 100,0,50)
-def hcurr = new H1F("hcurr", "current", 120,0,60)
-def hyieldcurr = new H2F("hyieldcurr", "yeild vs current", 120,0,60, 100,0,50)
-
-def ecounts = nelemap.sort{it.key[0]}.collect{[*it.key, it.value]}
-(0..ecounts.size-2).each{i->
-  def (dt,dfc0,dfc1,nele) = [*[0,1,2].collect{ecounts[i+1][it]-ecounts[i][it]}, ecounts[i][3]]
-
-  def yld = nele/dfc1
-  def curr = dfc1/dt/4*1e9
-
-  heyield.fill(yld)
-  hcurr.fill(curr)
-  hyieldcurr.fill(curr, yld)
-}
-
-def out = new TDirectory()
-out.mkdir("/root")
-out.cd("/root")
-out.addDataSet(heyield)
-out.addDataSet(hcurr)
-out.addDataSet(hyieldcurr)
-out.writeFile("ecounts_${outname}")
