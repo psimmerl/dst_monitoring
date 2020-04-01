01446879af1bf6d8656c7b73f5957bad372365c5
Andrey <kenjo@jlab.org>
2020-04-01 03:40:56 -0400

    update for ecounts

--- clas12/mon/electron/ECounts.groovy
+++ clas12/mon/electron/ECounts.groovy
@@ -1,5 +1,6 @@
 package clas12.mon.electron
 
+import org.jlab.detector.base.DetectorType
 import org.jlab.clas.physics.LorentzVector
 import org.jlab.groot.data.H1F
 import org.jlab.groot.data.H2F
@@ -22,64 +23,69 @@ class ECounts {
         fcentry[ts] = fcg
       }
 
-     if(event.hasBank("REC::Particle")) {
+     if(event.hasBank("REC::Particle") && event.hasBank("REC::Calorimeter")) {
         def partb = event.getBank("REC::Particle")
+        def calb = event.getBank("REC::Calorimeter")
         def iele = (0..<partb.rows()).find{partb.getInt('pid',it)==11 && partb.getShort("status",it)<0}
-        if(iele!=null) elentry[ts] = 0
+        def esec = (0..<calb.rows()).findResult{
+          (calb.getShort('pindex',it).toInteger() == iele &&
+          calb.getByte('detector',it).toInteger() == DetectorType.ECAL.getDetectorId()) ? calb.getByte('sector',it) : null
+        }
+
+        if(iele!=null && esec!=null) elentry[ts] = esec
       }
     }
   }
 
 
   def finish() {
-   def tline = fcentry.collect{ts,fcg->[ts,0,fcg]}
-   tline.addAll(elentry.collect{[it.key,1,0]})
-   tline.sort{a,b->a[0]<=>b[0]?:a[1]<=>b[1]}
+    def tline = fcentry.collect{ts,fcg->[ts,0,fcg]}
+    tline.addAll(elentry.collect{ts,esec->[ts,1,esec]})
+    tline.sort{a,b->a[0]<=>b[0]?:a[1]<=>b[1]}
 
-   def data = []
-   tline.each{ts,id,fcg->
-     if(id==0) data.add([ts, fcg, 0])
-     else if(data) data[-1][1]++
-   }
-   data.each{println it}
-   
-/*
-    def fc0 = null, fc1 = null, ts0 = null
-
-    def timeline = fcentry.sort().collect{ts,fcs->[ts,*fcs]}
-
-    def data = (0..timeline.size-2).collect{i->
-      def (dt,dfc0,dfc1) = [0,1,2].collect{timeline[i+1][it]-timeline[i][it]}
-      dt *= 4*1e-9
-      return [dt, dfc0, dfc1, dfc0/dt, dfc1/dt]
+    def data = []
+    tline.each{ts,id,val->
+      if(id==0) {
+        def fcg = val
+        if(data) {
+          def (ts0, fc0, nele) = data.last()
+          def dt = ts - ts0
+          def dq = fcg - fc0
+          data[-1].addAll([dt, dq, nele.collect{it/dq}])
+        }
+        data.add([ts,fcg,[0]*6])
+      } else if(data) {
+        def isec = val-1
+        data[-1][2][isec]++
+      }
     }
 
-    def h0fcdt = new H1F("hdt_0", "time between FC readings;time [sec]", 200,0,data.max{it[0]}[0])
-    def h0fcdq0 = new H1F("hugtdQ_0", "ungated charge between FC readings:charge [nC]", 300,0,data.max{it[1]}[1])
-    def h0fcdq1 = new H1F("hgtdQ_0", "gated charge between FC readings:charge [nC]", 300,0,data.max{it[2]}[2])
-    def h0curr0 = new H1F("hugtdI_0", "ungated current;current [nA]", 200,0,data.max{it[3]}[3])
-    def h0curr1 = new H1F("hgtdI_0", "gated current;current [nA]", 200,0,data.max{it[4]}[4])
-    def h1fcdt = new H1F("hdt_1", "time between FC readings;time [sec]", 200,0,0.1)
-    def h1fcdq0 = new H1F("hugtdQ_1", "charge between FC readings:charge [nC]", 300,0,3)
-    def h1fcdq1 = new H1F("hgtdQ_1", "charge between FC readings:charge [nC]", 300,0,3)
-    def h1curr0 = new H1F("hugtdI_1", "current;current [nA]", 300,0,60)
-    def h1curr1 = new H1F("hgtdI_1", "current;current [nA]", 300,0,60)
+    int maxnele = Math.max(20, data.collect{it[2].max()}.max().toInteger()+5)
+    def maxnorm = data.dropRight(1).collect{it[5].max()}.max()
+    def hsnele = (1..6).collect{new H2F("h2nele_s${it}", "number of electrons int sec $it between FC readings;number of electrons", maxnele,0,maxnele,200,0,60)}
+    def hsnorm0 = (1..6).collect{new H2F("full/h2enorm_s${it}", "normalized number of electrons in sec $it;normalized number of electrons", 200,0,maxnorm,200,0,60)}
+    def hsnorm1 = (1..6).collect{new H2F("zoom/h2enorm_s${it}", "normalized number of electrons in sec $it;normalized number of electrons", 200,0,15,200,0,60)}
 
-    data.each{dt,dfc0,dfc1,curr0,curr1->
-      h0fcdt.fill(dt)
-      h0fcdq0.fill(dfc0)
-      h0fcdq1.fill(dfc1)
-      h0curr0.fill(curr0)
-      h0curr1.fill(curr1)
+    maxnele = Math.max(50, data.collect{it[2].sum()}.max().toInteger()+5)
+    maxnorm = data.dropRight(1).collect{it[5].sum()}.max()
+    def h0nele = new H2F("h2nele", "number of electrons in all sectors between FC readings;number of electrons", maxnele,0,maxnele,200,0,60)
+    def h0norm0 = new H2F("full/h2enorm", "normalized number of electrons in all sectors;normalized number of electrons", 200,0,maxnorm,200,0,60)
+    def h0norm1 = new H2F("zoom/h2enorm", "normalized number of electrons in all sectors;normalized number of electrons", 200,0,40,200,0,60)
 
-      h1fcdt.fill(dt)
-      h1fcdq0.fill(dfc0)
-      h1fcdq1.fill(dfc1)
-      h1curr0.fill(curr0)
-      h1curr1.fill(curr1)
+    data.dropRight(1).each{ts,fcg,nele,dts,dfc,norm->
+      def curr = dfc/dts/4*1e9
+      (0..<6).each{hsnele[it].fill(nele[it],curr)}
+      (0..<6).each{hsnorm0[it].fill(norm[it],curr)}
+      (0..<6).each{hsnorm1[it].fill(norm[it],curr)}
+      h0nele.fill(nele.sum(),curr)
+      h0norm0.fill(norm.sum(),curr)
+      h0norm1.fill(norm.sum(),curr)
     }
 
-    [h0fcdt,h0fcdq0,h0fcdq1,h0curr0,h0curr1,h1fcdt,h1fcdq0,h1fcdq1,h1curr0,h1curr1].each{hists[it.getName()] = it}
-*/
+    [hsnele,hsnorm0,hsnorm1].each{it.each{hists[it.getName().replace("h2","h")] = it.projectionX(it.getName().replace("h2","h"))}}
+    [h0nele,h0norm0,h0norm1].each{hists[it.getName().replace('h2','h')] = it.projectionX(it.getName().replace("h2","h"))}
+
+    [hsnele,hsnorm0,hsnorm1].each{it.each{hists[it.getName()] = it}}
+    [h0nele,h0norm0,h0norm1].each{hists[it.getName()] = it}
   }
 }
