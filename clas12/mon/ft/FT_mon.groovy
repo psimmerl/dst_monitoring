730c7e2f8cbb30b701f536f141f547c1f0680198
Andrey <kenjo@jlab.org>
2020-04-08 11:48:17 -0400

    added internal run timeline to FT monitoring

--- clas12/mon/ft/FT_mon.groovy
+++ clas12/mon/ft/FT_mon.groovy
@@ -2,6 +2,7 @@ package clas12.mon.ft
 
 import org.jlab.detector.base.DetectorType
 import org.jlab.clas.physics.LorentzVector
+import org.jlab.groot.data.GraphErrors
 import org.jlab.groot.data.H1F
 import org.jlab.groot.data.H2F
 import java.util.concurrent.ConcurrentHashMap
@@ -11,6 +12,7 @@ class FT_mon {
   def hists = new ConcurrentHashMap()
   def fcmap = new ConcurrentHashMap()
   def trgmap = new ConcurrentHashMap()
+  def nqmap = new ConcurrentHashMap()
 
   def processEvent(event) {
     if(event.hasBank("RUN::config")) {
@@ -26,19 +28,14 @@ class FT_mon {
         fcmap[ts] = fcg
       }
 
-/*
-     if(event.hasBank("REC::Particle") && event.hasBank("REC::Calorimeter")) {
+      if(event.hasBank("REC::Particle")) {
         def partb = event.getBank("REC::Particle")
-        def calb = event.getBank("REC::Calorimeter")
-        def iele = (0..<partb.rows()).find{partb.getInt('pid',it)==11 && partb.getShort("status",it)<0}
-        def esec = (0..<calb.rows()).findResult{
-          (calb.getShort('pindex',it).toInteger() == iele &&
-          calb.getByte('detector',it).toInteger() == DetectorType.ECAL.getDetectorId()) ? calb.getByte('sector',it) : null
-        }
+        def ifts = (0..<partb.rows()).findAll{partb.getShort("status",it).with{it>=1000 && it<2000} && Math.sqrt(['px','py','pz'].sum{comp->partb.getFloat(comp,it)**2})>0.3}
+        int nneuts = ifts.findAll{partb.getByte('charge',it)==0}.size()
+        int ncharged = ifts.size() - nneuts
 
-        if(iele!=null && esec!=null) elentry[ts] = esec
+        nqmap[ts] = [nneuts, ncharged]
       }
-*/
     }
   }
 
@@ -46,6 +43,7 @@ class FT_mon {
   def finish() {
     def tline = fcmap.collect{ts,fcg->[ts,0,fcg]}
     tline.addAll(trgmap.collect{ts,trgs->[ts,1,trgs]})
+    tline.addAll(nqmap.collect{ts,qqs->[ts,2,qqs]})
     tline.sort{a,b->a[0]<=>b[0]?:a[1]<=>b[1]}
 
     def data = []
@@ -55,27 +53,43 @@ class FT_mon {
 
         if(data) {
           data[-1].dt = ts - data[-1].ts
+          data[-1].tt = ts - data[0].ts
           data[-1].dq = fcg - data[-1].fc
           data[-1].norm = data[-1].with{ntrg*.div(dq)}
         }
 
-        data.add([fc:fcg, ts:ts, ntrg:[0,0]])
-      } else if(data) {
+        data.add([fc:fcg, ts:ts, ntrg:[0,0], nqqs: [0,0]])
+      } else if(id==1 && data) {
         data[-1].ntrg[0] += val[0]
         data[-1].ntrg[1] += val[1]
+      } else if(id==2 && data) {
+        data[-1].nqqs[0] += val[0]
+        data[-1].nqqs[1] += val[1]
       }
     }
 
+    def grnqqs = [new GraphErrors("grnqqs0"), new GraphErrors("grnqqs1")]
+    def grnorm = [new GraphErrors("grnorm24"), new GraphErrors("grnorm25")]
+    data.dropRight(1).groupBy{it.tt/4/1e9/30}.each{t0,evlist->
+      grnorm[0].addPoint(t0*30, evlist.sum{it.norm[0]}/evlist.size(), 0,0)
+      grnorm[1].addPoint(t0*30, evlist.sum{it.norm[1]}/evlist.size(), 0,0)
+      grnqqs[0].addPoint(t0*30, evlist.sum{it.nqqs[0]}/evlist.size(), 0,0)
+      grnqqs[1].addPoint(t0*30, evlist.sum{it.nqqs[1]}/evlist.size(), 0,0)
+    }
+
+    def maxnqqs = (0..1).collect{i->data.max{it.nqqs[i]}.nqqs[i].toInteger()+5}.collect{[it,0,it]}
     def maxntrg = (0..1).collect{i->data.max{it.ntrg[i]}.ntrg[i].toInteger()+5}.collect{[it,0,it]}
     def maxnorm = (0..1).collect{i->(data.dropRight(1).max{it.norm[i]}.norm[i].toInteger()+5).with{[it*(i?:10),0,it]}}
     def fxdmax = [24:15, 25:200]
 
+    def hsnqqs = [0,1].collect{new H2F("h2nqqs_${it}", "number of ${it?'charged':'neutrals'} between FC readings;number of particles", *maxnqqs[it],200,0,60)}
     def hsntrgs = [24,25].collect{new H2F("h2ntrg_b${it}", "number of ${it}th trigger bits between FC readings;number of trigger bits", *maxntrg[it-24],200,0,60)}
     def hsnorm0 = [24,25].collect{new H2F("full/h2norm0_b${it}", "normalized number of ${it}th trigger bits [full range];normalized number of trigger bits",*maxnorm[it-24],200,0,60)}
     def hsnorm1 = [24,25].collect{new H2F("fixed/h2norm1_b${it}", "normalized number of ${it}th trigger bits [fixed range];normalized number of trigger bits", 200,0,fxdmax[it],200,0,60)}
 
     data.dropRight(1).each{
       def curr = it.dq/it.dt/4*1e9
+      it.nqqs.eachWithIndex{nn,i->hsnqqs[i].fill(nn,curr)}
       it.ntrg.eachWithIndex{nn,i->hsntrgs[i].fill(nn,curr)}
       it.norm.eachWithIndex{nn,i->
         hsnorm0[i].fill(nn,curr)
@@ -83,7 +97,7 @@ class FT_mon {
       }
     }
 
-    [hsntrgs,hsnorm0,hsnorm1].each{it.each{hists[it.getName().replace("h2","h")] = it.projectionX(it.getName().replace("h2","h"), it.getTitle())}}
-    [hsntrgs,hsnorm0,hsnorm1].each{it.each{hists[it.getName()] = it}}
+    [hsnqqs, hsntrgs,hsnorm0,hsnorm1].each{it.each{hists[it.getName().replace("h2","h")] = it.projectionX(it.getName().replace("h2","h"), it.getTitle())}}
+    [hsnqqs, hsntrgs,hsnorm0,hsnorm1,grnorm,grnqqs].each{it.each{hists[it.getName()] = it}}
   }
 }
