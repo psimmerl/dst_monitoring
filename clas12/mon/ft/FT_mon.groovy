3cda43138f287ff6508f1563562cff7bb82350da
Andrey <kenjo@jlab.org>
2020-04-15 23:29:49 -0400

    added internal timeline as H2F for FT

--- clas12/mon/ft/FT_mon.groovy
+++ clas12/mon/ft/FT_mon.groovy
@@ -66,19 +66,17 @@ class FT_mon {
       }
     }
 
-    def grnqqs = [new GraphErrors("grnqqs0"), new GraphErrors("grnqqs1")]
-    def grnorm = [new GraphErrors("grnorm24"), new GraphErrors("grnorm25")]
-    data.dropRight(1).groupBy{(it.tt*4/1e9/30).toInteger()}.each{tt,evlist->
-      grnorm[0].addPoint(tt*30, evlist.isEmpty()?0:evlist.sum{it.norm[0]}/evlist.size(), 0,0)
-      grnorm[1].addPoint(tt*30, evlist.isEmpty()?0:evlist.sum{it.norm[1]}/evlist.size(), 0,0)
-      grnqqs[0].addPoint(tt*30, evlist.isEmpty()?0:evlist.sum{it.nqqs[0]}/evlist.size(), 0,0)
-      grnqqs[1].addPoint(tt*30, evlist.isEmpty()?0:evlist.sum{it.nqqs[1]}/evlist.size(), 0,0)
-    }
-
     def maxnqqs = (0..1).collect{i->data.max{it.nqqs[i]}.nqqs[i].toInteger()+5}.collect{[it,0,it]}
     def maxntrg = (0..1).collect{i->data.max{it.ntrg[i]}.ntrg[i].toInteger()+5}.collect{[it,0,it]}
     def maxnorm = (0..1).collect{i->(data.dropRight(1).max{it.norm[i]}.norm[i].toInteger()+5).with{[it*(i?:10),0,it]}}
     def fxdmax = [24:15, 25:200]
+
+    def mints = Math.floor(data.min{it.ts}.ts*4*1e-9).toInteger()
+    def maxts = Math.ceil(data.max{it.ts}.ts*4*1e-9).toInteger()
+    def tsbins = [maxts-mints,mints,maxts]
+
+    def h2tnqqs = [0,1].collect{new H2F("h2tnqqs_${it}", "number of ${it?'charged':'neutrals'} between FC readings;timestamp;number of particles", *tsbins, *maxnqqs[it])}
+    def h2tnorm0 = [24,25].collect{new H2F("full/h2tnorm0_b${it}", "normalized number of ${it}th trigger bits [full range];timestamp;normalized number of trigger bits", *tsbins, *maxnorm[it-24])}
 
     def hsnqqs = [0,1].collect{new H2F("h2nqqs_${it}", "number of ${it?'charged':'neutrals'} between FC readings;number of particles", *maxnqqs[it],200,0,60)}
     def hsntrgs = [24,25].collect{new H2F("h2ntrg_b${it}", "number of ${it}th trigger bits between FC readings;number of trigger bits", *maxntrg[it-24],200,0,60)}
@@ -86,16 +84,19 @@ class FT_mon {
     def hsnorm1 = [24,25].collect{new H2F("fixed/h2norm1_b${it}", "normalized number of ${it}th trigger bits [fixed range];normalized number of trigger bits", 200,0,fxdmax[it],200,0,60)}
 
     data.dropRight(1).each{
+      def t0 = it.ts*4*1e-9
       def curr = it.dq/it.dt/4*1e9
       it.nqqs.eachWithIndex{nn,i->hsnqqs[i].fill(nn,curr)}
+      it.nqqs.eachWithIndex{nn,i->h2tnqqs[i].fill(t0,nn)}
       it.ntrg.eachWithIndex{nn,i->hsntrgs[i].fill(nn,curr)}
       it.norm.eachWithIndex{nn,i->
+        h2tnorm0[i].fill(t0,nn)
         hsnorm0[i].fill(nn,curr)
         hsnorm1[i].fill(nn,curr)
       }
     }
 
     [hsnqqs, hsntrgs,hsnorm0,hsnorm1].each{it.each{hists[it.getName().replace("h2","h")] = it.projectionX(it.getName().replace("h2","h"), it.getTitle())}}
-    [hsnqqs, hsntrgs,hsnorm0,hsnorm1,grnorm,grnqqs].each{it.each{hists[it.getName()] = it}}
+    [h2tnorm0,h2tnqqs,hsnqqs, hsntrgs,hsnorm0,hsnorm1].each{it.each{hists[it.getName()] = it}}
   }
 }
